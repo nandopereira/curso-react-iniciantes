c9b708bd85747b1dd0f50042b1b77f19a8011e87
nandopereira <74563514+nandopereira@users.noreply.github.com>
2022-05-25 11:29:17 -0300

    Initial commit

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./navbar.css";
+export default function navbar() {
+  return (
+    <nav className="navbar">
+      <span>Kanban React</span>
+    </nav>
+  );
+}
